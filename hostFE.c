cd8cac887386aa01dfc2907667f08cf7700abb30
tim00631 <tim00631@gmail.com>
2021-01-06 07:26:05 +0000

    first commit

---
+++ hostFE.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hostFE.h"
+#include "helper.h"
+#define CL_KERNEL_NAME "convolution"
+void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
+            float *inputImage, float *outputImage, cl_device_id *device,
+            cl_context *context, cl_program *program)
+{
+    cl_int status;
+    int filterSize = filterWidth * filterWidth;
+
+    cl_kernel kernel = clCreateKernel(*program, CL_KERNEL_NAME, &status);
+    CHECK(status, "clCreateKernel");
+    cl_mem inputImageBuffer = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * imageHeight * imageWidth, inputImage, &status);
+    CHECK(status, "clCreateBuffer for input");
+    cl_mem filterBuffer = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * filterSize, filter, &status);
+    CHECK(status, "clCreateBuffer for filter");
+    cl_mem outputImageBuffer = clCreateBuffer(*context, CL_MEM_WRITE_ONLY, sizeof(cl_float) * imageHeight * imageWidth, NULL, &status);
+    CHECK(status, "clCreateBuffer for output");
+    cl_command_queue queue = clCreateCommandQueue(*context, *device, 0, &status);
+    CHECK(status, "clCreateCommandQueue");
+    
+    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputImageBuffer);
+    status |=clSetKernelArg(kernel, 1, sizeof(cl_mem), &filterBuffer);
+    status |=clSetKernelArg(kernel, 2, sizeof(cl_mem), &outputImageBuffer);
+    status |=clSetKernelArg(kernel, 3, sizeof(cl_float), &imageWidth);
+    status |=clSetKernelArg(kernel, 4, sizeof(cl_float), &filterWidth);
+    CHECK(status, "clSetKernelArg");
+
+    const size_t global_work_size [2] = {imageWidth, imageHeight};
+    const size_t local_work_size [2] = {8 ,8};
+    status = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_work_size, local_work_size, 0, NULL, NULL);
+    CHECK(status, "clEnqueueNDRangeKernel");
+    status = clEnqueueReadBuffer(queue, outputImageBuffer, CL_TRUE, 0, sizeof(float)* imageWidth * imageHeight, outputImage, 0, NULL, NULL);
+
+    // Release memory
+    clReleaseCommandQueue(queue);
+}
---
+++ kernel.cl
@@ -0,0 +1,30 @@
+__kernel void convolution(
+    const __global float * inputImage,
+    const __global float * filter,
+    __global float * outputImage,
+    const int imageWidth,
+    const int imageHeight,
+    const int filterWidth)
+{
+    // const int id = get_global_id(0);
+    int i = get_global_id(0); // horizontal
+    int j = get_global_id(1); // vertical
+    // printf("[%d][%d]\n", j, i);
+    int halffilterSize = filterWidth / 2;
+    float sum = 0;
+    // printf("halffilterSize:%d\n", halffilterSize);
+    for (int l = -halffilterSize; l <= halffilterSize; l++) // vertical
+    {
+        // const int idxIntmp = (j + l) * imageWidth + i;
+        for (int k = -halffilterSize; k <= halffilterSize; k++) // horizontal
+        {
+            if (i + k >= 0 && i + k < imageWidth && j + l >= 0 && j + l < imageHeight)
+            {
+                sum += inputImage[(j + l) * imageWidth + i + k] * filter[(l + halffilterSize) * filterWidth + k + halffilterSize];
+                    // printf("inputImage[(j + l) * imageWidth + i + k]:%f\n", inputImage[(j + l) * imageWidth + i + k]);
+            }
+        }
+    }
+    outputImage[j * imageWidth + i] = sum;
+    // printf("global_id:%d output[%d][%d]:%d\n", id, j, i, output[j * imageWidth + i]);
+}
